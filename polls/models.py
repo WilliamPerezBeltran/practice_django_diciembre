22300497502e7e5cc9742e9869fff455c47d55e6
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-11-28 16:18:00 -0500

    adaf

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class IdentificationDocument(models.Model):
+    name = models.CharField(max_length=200)
+
+
+
+class Study(models.Model):
+    name = models.CharField(max_length=200)
+
+
+
+class MaritalStatus(models.Model):
+    name = models.CharField(max_length=200)
+
+class User(models.Model):
+	names = models.CharField(max_length=256,null=False)
+	marital_status = models.ForeignKey(MaritalStatus, on_delete=models.CASCADE)
+	# identification_document = models.ForeignKey(IdentificationDocument, on_delete=models.CASCADE)
+	# study = models.ForeignKey(Study, on_delete=models.CASCADE)
+	first_last_name = models.CharField(max_length=256,null=False)
+	second_last_name = models.CharField(max_length=256,null=False)
+	number_identification = models.CharField(max_length=256,null=False)
+	birth_date = models.DateField(blank=True, null=True)
+	gender = models.CharField(max_length=256,null=False)
+	residence_address = models.CharField(max_length=256,null=False)
+	main_phone = models.CharField(max_length=256,null=False)
+	secondary_phone = models.CharField(max_length=256,null=False)
+	email = models.EmailField(max_length=256,null=False)
+
