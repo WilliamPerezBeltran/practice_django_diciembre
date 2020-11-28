52ea02afc8b8349684da9b8133f6fdd0f22645c2
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-11-28 17:56:47 -0500

    add models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import IdentificationDocument,Study,MaritalStatus,User
+
+admin.site.register(IdentificationDocument)
+admin.site.register(Study)
+admin.site.register(MaritalStatus)
+admin.site.register(User)
+
--- polls/models.py
+++ polls/models.py
@@ -2,23 +2,33 @@ from django.db import models
 
 
 class IdentificationDocument(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.TextField(max_length=200, default=" ")
+
+    def __str__(self):
+    	return self.name
 
 
 
 class Study(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.TextField(max_length=200, default=" ")
+
+    def __str__(self):
+    	return self.name
 
 
 
 class MaritalStatus(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.TextField(max_length=200, default=" ")
+
+    def __str__(self):
+    	return self.name
+    	
 
 class User(models.Model):
 	names = models.CharField(max_length=256,null=False)
 	marital_status = models.ForeignKey(MaritalStatus, on_delete=models.CASCADE)
-	# identification_document = models.ForeignKey(IdentificationDocument, on_delete=models.CASCADE)
-	# study = models.ForeignKey(Study, on_delete=models.CASCADE)
+	identification_document = models.ForeignKey(IdentificationDocument, on_delete=models.CASCADE)
+	study = models.ForeignKey(Study, on_delete=models.CASCADE)
 	first_last_name = models.CharField(max_length=256,null=False)
 	second_last_name = models.CharField(max_length=256,null=False)
 	number_identification = models.CharField(max_length=256,null=False)
@@ -28,4 +38,7 @@ class User(models.Model):
 	main_phone = models.CharField(max_length=256,null=False)
 	secondary_phone = models.CharField(max_length=256,null=False)
 	email = models.EmailField(max_length=256,null=False)
+
+	def __str__(self):
+		return self.names
 
